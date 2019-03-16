55f693a30abe2e20280e1f77b8b787c7c43113aa
imaharu <light1200out@gmail.com>
2019-03-16 22:29:33 +0900

    Done step6

---
+++ db/migrate/20190316130251_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :context
+      t.datetime :closed_on
+      t.integer :status, default: 1
+      t.integer :priority, default: 0 # 0は未設定
+
+      t.timestamps
+    end
+  end
+end
