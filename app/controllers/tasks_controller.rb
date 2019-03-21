3a3c47b7555d1be72e867c17f735b1220260b1a0
imaharu <light1200out@gmail.com>
2019-03-21 16:05:16 +0900

    Changeb erb to haml

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,24 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+      @tasks = Task.all
     end
 
     def show
-        @task = Task.find(params[:id])
+      @task = Task.find(params[:id])
+    end
+
+    def new
+      @task = Task.new
+    end
+
+    def create
+      task = Task.new(task_params)
+      task.save
+      redirect_to tasks_url, notice: "タスクの「#{task.name}」を登録しました。"
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:name, :context, :closed_on)
     end
 end
