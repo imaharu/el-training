f03e207926dfd8d48e751acbfc6e7f7ffd361eea
imaharu <light1200out@gmail.com>
2019-03-21 21:11:13 +0900

    Start rspec

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module ElTraining
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.generators do |g|
-      g.test_framework false
+      g.test_framework :rspec
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
